31ea8fbdefaf539d728ecd493dea054740cd10e7
frimpongopoku <pongofrimi@gmail.com>
2021-04-16 12:34:18 +0400

    NIcely setup circle and clear button, plus hanging name box that will collect user name

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "./../App.css";
+
+export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="up-div">
+          <h1>WELCOME TO PURPLE GAMES</h1>
+          <h3>
+            Think you are a CSS champion? <br /> Lets Find Out!
+          </h3>
+        </div>
+
+        {/* <div className="circle lift"></div> */}
+        <div className="name-area lift">
+          <h3>TYPE YOUR FULL NAME IN THE BOX BELOW</h3>
+          <input className="name-box" placeholder="Enter name..." />
+        </div>
+        <div className="down-div"></div>
+      </div>
+    );
+  }
+}
