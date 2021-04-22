94c740d9e6c5cbadb5a6a21e8d4ffb4d07b8d8d5
frimpongopoku <pongofrimi@gmail.com>
2021-04-22 16:42:13 +0400

    Starting fxnality setup

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,15 +1,35 @@
 import { faLongArrowAltRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { Component } from "react";
+import { withRouter } from "react-router";
 import "./../App.css";
 
-export default class LandingPage extends Component {
+class LandingPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      showCircle: true,
+      start: false,
+      name: "",
+    };
   }
 
+  handleTextBox = (e) => {
+    e.preventDefault();
+    this.setState({ name: e.target.value.trim() });
+  };
+
+  readyUpAndStart = (e) => {
+    e.preventDefault();
+    const { name} = this.state; 
+    if(!name) {alert("Please provide a valid name in order to start the game"); return; }
+    this.props.history.push({ 
+      pathname:"/play", 
+      data: { userName: this.state.name}
+    }) 
+  };
   render() {
+    const { showCircle, start } = this.state;
     return (
       <div className="c-container">
         <div className="up-div">
@@ -19,16 +39,29 @@ export default class LandingPage extends Component {
           </h3>
         </div>
 
-        {/* <div className="circle lift"></div> */}
-        <div className="name-area lift">
-          <h3>TYPE YOUR FULL NAME IN THE BOX BELOW</h3>
-          <input className="name-box" placeholder="Enter name..." />
-          <button className="go-btn">
-            <FontAwesomeIcon icon={faLongArrowAltRight} />
-          </button>
-        </div>
+        {showCircle && (
+          <div
+            className="circle lift"
+            onClick={() => this.setState({ showCircle: false, start: true })}
+          ></div>
+        )}
+        {start && (
+          <div className="name-area lift">
+            <h3>TYPE YOUR FULL NAME IN THE BOX BELOW</h3>
+            <input
+              className="name-box"
+              placeholder="Enter name..."
+              onChange={this.handleTextBox}
+            />
+            <button className="go-btn" onClick={(e)=>this.readyUpAndStart(e)}>
+              <FontAwesomeIcon icon={faLongArrowAltRight} />
+            </button>
+          </div>
+        )}
         <div className="down-div"></div>
       </div>
     );
   }
 }
+
+export default withRouter(LandingPage);
